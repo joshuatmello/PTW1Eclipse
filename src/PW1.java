6aac8cd9eca66b653cd33d2ab72de311ad5c7c8b
joshuatmello <joshuatmello@gmail.com>
2022-12-22 18:30:23 -0800

    first commit

---
+++ src/PW1.java
@@ -0,0 +1,13 @@
+
+public class PW1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 34;
+		int ageMarried = 28;
+		int yearsMarried = age - ageMarried;
+		System.out.println(yearsMarried);
+	}
+
+}
---
+++ src/PracticeProj.java
@@ -0,0 +1,14 @@
+
+public class PracticeProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Practice coding
+		
+		int age= 34;
+		int marriageAge= 28;
+		int yearsMarried= age - marriageAge; 
+		System.out.println("I have been married for this many years");
+	}
+
+}
